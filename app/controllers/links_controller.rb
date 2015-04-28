f1a3b20fb3025a454f46a610255ac2d5758f20e6
Rafael Tejado <rtejado90@gmail.com>
2015-04-28 20:29:54 +0200

    Create method to get random short url

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,8 +6,14 @@ class LinksController < ApplicationController
   end
 
   def new
+  	@link = Link.new
   end
 
   def edit
+  end
+
+  def create
+  	@link = Link.new
+  	@link.shortlink = generate_short_link
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,25 @@
 class Link < ActiveRecord::Base
 	validates :link, uniqueness: true, presence: true
+	validates :shortlink, uniqueness: true, presence: true
+
+	def generate_short_link(characters)
+		chain = false
+		while Link.find_by shortlink: chain
+			chain = gen_random_chain(characters)
+		end
+
+		self.shortlink = chain
+	end
+
+	def gen_random_chain(characters)
+		chain =""
+		characters.times do |n|
+			character = rand(48..122)
+			character = rand(48..122) while (58..64).include?(character) || (91..97).include?(character)
+			chain << character.chr
+		end
+		chain
+	end
+
+
 end
